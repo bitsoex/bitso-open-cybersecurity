8ab550c6fd4a48d5cb778354adc082ddad492e04
Luis Servando Rios Baena <luis.rios@bitso.com>
2024-09-26 13:42:19 -0600

    pushing nerdearla code

--- security-lambdas/provider.tf
+++ security-lambdas/provider.tf
@@ -1,10 +1,3 @@
 provider "aws" {
   region = "us-east-2"
-  assume_role {
-    role_arn     = "arn:aws:iam::183832830806:role/aws-provisioner"
-    session_name = "mysession"
-  }
-  default_tags {
-    tags = local.default_tags
-  }
 }
